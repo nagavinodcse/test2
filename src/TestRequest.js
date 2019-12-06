afad5c48db188ddec8b4583e975aab2e6afcd1be
Naga Vinod <nagavinod424@gmail.com>
2019-12-06 20:08:19 +0530

    Showing the checkbox tree in progress

--- src/TestRequest.js
+++ src/TestRequest.js
@@ -1,28 +1,56 @@
-import React, {Component} from 'react';
+import React, {Component,useState} from 'react';
 import {Accordion, Button, Card} from "react-bootstrap";
-import divWithClassName from "react-bootstrap/esm/divWithClassName";
-
-/**
- * @return {string}
- */
-const TestTypes = ({selectedOption}) => selectedOption !== '' ?
-    <div>
-        <h1>{selectedOption }</h1>
-        <Accordion defaultActiveKey="" className="mt-3">
-            <Card bg="secondary">
-                <Accordion.Toggle as={Card.Header} eventKey="0">&gt;&gt; WSH </Accordion.Toggle>
+import { TreeViewComponent } from '@syncfusion/ej2-react-navigations';
+import "@syncfusion/ej2-base/styles/material.css";
+import "@syncfusion/ej2-react-navigations/styles/material.css";
+import "@syncfusion/ej2-inputs/styles/material.css";
+import "@syncfusion/ej2-buttons/styles/material.css";
+const OSTypes = ({showOSDiv}) =>{
+    let os = [
+            { id: 1, name: 'RS1', hasChild: true, expanded: true },
+            { id: 2, name: 'RS2', hasChild: true, expanded: true },
+            { id: 3, pid: 1, name: 'x86', isChecked: false },
+            { id: 4, pid: 1, name: 'x64',isChecked:false },
+            { id: 5, pid: 2, name: 'x86',isChecked:false },
+            { id: 6, pid: 2, name: 'x64', isChecked: false },
+        ];
+    const [operatingSystems,setOperatingSystems] = useState(os);
+    const [isChecked,setIsChecked] = useState(true);
+    return (showOSDiv) ?
+        <div className="mt-3">
+            <TreeViewComponent fields={operatingSystems} showCheckBox={isChecked}/>);
+        </div> : '';
+};
+const TestTypes = ({selectedOption}) => {
+    const [showOSDiv, setOSDiv] = useState(false);
+    return selectedOption !== '' ?
+        <div>
+            <h1>{selectedOption}</h1>
+            <Accordion defaultActiveKey="" className="mt-3">
+                <Card bg="secondary">
+                    <Accordion.Toggle as={Card.Header} eventKey="0">&gt;&gt; WSH </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
-                        <Card.Body>Hello! I'm the body</Card.Body>
+                        <Card.Body>
+                            <a href="#" onClick={()=>setOSDiv(true)}>Select an Operating System</a>
+                            <OSTypes showOSDiv={showOSDiv}/>
+                        </Card.Body>
                     </Accordion.Collapse>
-            </Card>
-            <Card bg="secondary">
-                <Accordion.Toggle as={Card.Header} eventKey="1">&gt;&gt; Web Crawler </Accordion.Toggle>
-                <Accordion.Collapse eventKey="1">
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                </Accordion.Collapse>
-            </Card>
-        </Accordion>
-    </div> : '';
+                </Card>
+                <Card bg="secondary">
+                    <Accordion.Toggle as={Card.Header} eventKey="1">&gt;&gt; Web Crawler </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="1">
+                        <Card.Body>
+                        <ul>
+                            <li>Top 100 Sites</li>
+                            <li>Top 50 Sites</li>
+                        </ul>
+                    </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                {/*<OSTypes showOSDiv={showOSDiv}/>*/}
+            </Accordion>
+        </div> : '';
+};
 
 class TestRequest extends Component {
     state = {
